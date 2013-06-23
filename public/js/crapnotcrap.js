877b54186f0e171a440b8e23ae65b0e1d85d6166
Charles Gravolet <cgravolet@gmail.com>
2013-06-23 17:49:26 -0400

    Updated layout and styles, added autoFocus method when no results are displayed

--- public/js/crapnotcrap.js
+++ public/js/crapnotcrap.js
@@ -1,4 +1,16 @@
-(function ($) {}(jQuery));
+(function ($) {
+
+	function autoFocusNoResultsForm() {
+		if ($(".no-results").length) {
+			$("input[name=search]").focus();
+		}
+	}
+
+	$(function () {
+		autoFocusNoResultsForm();
+	});
+
+}(jQuery));
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
